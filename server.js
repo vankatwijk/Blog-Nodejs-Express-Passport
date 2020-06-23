fb363f3e4aca1a2eff4d9453030bcf284e336621
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-23 11:10:55 +0200

    initial view with ejs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) =>{
-    res.send('Hello World')
+    res.render('index')
 })
 
 app.listen(5000)
