d422fb32d6b6b4823d320f7cffb58b39964034ef
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-23 11:59:36 +0200

    add mongodb

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+mongoose.connect('mongodb://localhost/nodeblog',{
+    useNewUrlParser: true, useUnifiedTopology: true
+});
 
 app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
