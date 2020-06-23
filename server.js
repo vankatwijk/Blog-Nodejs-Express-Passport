7fc71c8a3c5cacdc77850ea5cc7e38a999984407
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-23 11:32:33 +0200

    displaying articles on the home page

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) =>{
-    res.render('index')
+    const articles =[{
+        title: 'test article',
+        createdAt: Date.now(),
+        description: 'test description'
+    },
+    {
+        title: 'test article2',
+        createdAt: Date.now(),
+        description: 'test description2'
+    }]
+    res.render('index', {articles: articles})
 })
 
 app.listen(5000)
