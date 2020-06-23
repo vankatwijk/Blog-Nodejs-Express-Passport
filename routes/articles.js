a6b9ec5fb5faab08b6f17591944b7219f929f910
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-23 13:09:02 +0200

    edit functionality added

--- routes/articles.js
+++ routes/articles.js
@@ -7,8 +7,9 @@ router.get('/new', (req, res) => {
 })
 
 
-router.get('/edit/:id', (req, res) => {
-    res.render('articles/new',{article: new Article() })
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
 })
 
 router.get('/:slug', async (req, res) => {
@@ -31,9 +32,30 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
+  
+
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+      let article = req.article
+      article.title = req.body.title
+      article.description = req.body.description
+      article.markdown = req.body.markdown
+      try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`)
+      } catch (e) {
+        res.render(`articles/${path}`, { article: article })
+      }
+    }
+}
 
 module.exports = router
